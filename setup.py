911181a732d1bb3b0065aafb079fe85ade9d978a
Xuan <xuan.physics@gmail.com>
2024-10-19 23:20:00 +0200

    Updating setup

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name='demoscode',
     version='0.1',
-    packages=find_packages(where='demos_web/services'),
-    package_dir={'': 'demos_web/services'},
+    packages=find_packages(where='web/services'),
+    package_dir={'': 'web/services'},
     include_package_data=True,
     install_requires=[
         # Aquí puedes listar las dependencias, si las hay
