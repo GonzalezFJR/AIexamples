0fd4c156f6df215865dcc7c04b1a7d56fdfb7b29
GonzalezFJR <xuancern@gmail.com>
2024-10-19 08:50:48 +0200

    Moving scripts to examples

--- scripts/example_visualize_layers.py
+++
@@ -1,7 +0,0 @@
-from torchvision import models
-from model_layers_visualizer import visualize
-
-if __name__ == '__main__':
-    model = models.alexnet()
-    input_shape = (1, 3, 224, 224)
-    visualize(model, input_shape, 'localhost', 8000)
